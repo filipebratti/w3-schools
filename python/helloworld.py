84363363d915558f7fc2ae25968a4540a2bf270e
filipebratti <filipebratti@hotmail.com>
2024-02-22 23:46:14 -0300

    python studies

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,1 +1,57 @@
 print("Hello, World!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+# This is a comment
+    
+if 5 > 3:
+    print("Five is greater than three!") 
+if 5 > 3:
+    print("Five is greater than three!")
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+a = 4
+A = "Sally"
+#A will not overwrite a 
+
+myvar = "John"
+my_var= "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
