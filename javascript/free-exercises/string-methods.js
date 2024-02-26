55d0ab619274ceef996cdeb097bb3b704c0e7fa0
filipebratti <filipebratti@hotmail.com>
2024-02-26 10:21:22 -0300

    adding comments

--- javascript/free-exercises/string-methods.js
+++ javascript/free-exercises/string-methods.js
@@ -1,6 +1,9 @@
 var text = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
 
+// .length retorna a quantidade de caracteres que a string possui
 console.log(text.length);
+
+// charAt() retorna o caracter iniciando do zero
 console.log(text.charAt(0));
 console.log(text.charAt(5));
 
@@ -8,13 +11,16 @@ if (text.charAt(5) === "F") {
     console.log("É a letra inicial do meu nome!")
 }
 
+// charCodeAt() retorna um número UTF-16 inteiro entre 0 e 65535
 console.log(text.charCodeAt(0));
 console.log(text.charCodeAt(5));
 
+// charAt() e text[] retornam o caracter da próxima posição na String
 console.log(text.charAt(0));
 console.log(text.charAt(5));
 
-console.log()
+console.log(text[0]);
+console.log(text[5]);
 
 
 
