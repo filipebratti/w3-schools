754ba90e20c102174c66dcfdff7c673650a610e7
filipebratti <filipebratti@hotmail.com>
2024-02-26 10:10:09 -0300

    monday morning exercises

---
+++ javascript/free-exercises/string-methods.js
@@ -0,0 +1,20 @@
+var text = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+
+console.log(text.length);
+console.log(text.charAt(0));
+console.log(text.charAt(5));
+
+if (text.charAt(5) === "F") {
+    console.log("É a letra inicial do meu nome!")
+}
+
+console.log(text.charCodeAt(0));
+console.log(text.charCodeAt(5));
+
+console.log(text.charAt(0));
+console.log(text.charAt(5));
+
+console.log()
+
+
+
