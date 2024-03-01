6d3cb5d0badc4f07509853e0867e6ee1ec47ba5b
filipebratti <filipebratti@hotmail.com>
2024-03-01 13:55:38 -0300

    python functions

---
+++ javascript/free-exercises/py-functions.py
@@ -0,0 +1,27 @@
+def my_function():
+    print("Hello from a funciton!")    
+my_function()
+
+def my_name():
+    print("My name is Filipe")
+my_name()
+
+def my_car():
+    print("I have a Audi E-tron GT")
+my_car()
+
+def my_city():
+    print("I live in Lages/SC")
+my_city()
+
+def your_name(fname):
+    print(fname + " Refsnes")
+your_name("Filipe")
+your_name("Juliana")
+your_name("Henrique")
+
+def full_name(fname, lname):
+    print(fname + " " + lname)
+full_name("Filipe", "Bratti")
+full_name("Juliana", "Duarte")
+full_name("Henrique", "Duarte")
