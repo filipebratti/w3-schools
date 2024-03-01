430f002c69e8712b59bf0d9591619c11ac1ad4a2
filipebratti <filipebratti@hotmail.com>
2024-02-29 23:57:48 -0300

    more functions

--- javascript/free-exercises/functions-1.js
+++ javascript/free-exercises/functions-1.js
@@ -3,7 +3,24 @@ function myName(name, lastName) {
     var lastName = "Bratti";
     return "Meu nome é " + name + " " + lastName;
 }
-
 console.log(myName())
 
+const x = function (a, b) {
+    return a * b;
+}
+let z = x(4, 3)
+console.log(z);
 
+const addFunction = function (a, b) {
+    return a + b;
+}
+let myFirstAdd = addFunction(5, 5);
+let mySecondAdd = addFunction(10, 10);
+console.log(myFirstAdd, mySecondAdd);
+
+const subFunction = function (a, b) {
+    return a - b;
+}
+let myFirstSub = subFunction(5, 2);
+let mySecondSub = subFunction(15, 3);
+console.log(myFirstSub, mySecondSub);
