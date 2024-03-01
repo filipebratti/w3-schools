1d638a1044ded2afcb50691e78911e01fc6a4d25
filipebratti <filipebratti@hotmail.com>
2024-03-01 20:59:38 -0300

    ternary operator

---
+++ javascript/free-exercises/operador-ternario.js
@@ -0,0 +1,11 @@
+studentAverage = 8.5;
+ studentAverage >= 7 ? "Aproved" : "Reproved";
+
+studentAverage2 = 6.3;
+studentAverage2 >= 7 ? "Aproved" : "Reproved";
+
+var accountBalance = 1.000;
+accountBalance > 0 ? "Positive Balance" : "Negative Balance";
+
+var accountBalance2 = -20;
+accountBalance2 > 0 ? "Positive Balance" : "Negative Balance";
