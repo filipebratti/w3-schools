89cb8ddac4282ae8f448055318fc86d2c7462b3d
filipebratti <filipebratti@hotmail.com>
2024-03-01 11:53:16 -0300

    user class

--- javascript/free-exercises/classes.js
+++ javascript/free-exercises/classes.js
@@ -17,3 +17,24 @@ class VideoGame {
 }
 const myVideoGame = new VideoGame('Xbox', 'Xbox Series S', 'Forza Horizon')
 console.log(myVideoGame.bestgame, '/', myVideoGame.model, '/', myVideoGame.bestgame)
+
+class User {
+    constructor(id, fullName, birthDate, age, gender, cellPhone, country, state, city, cep, street, houseNumber) {
+        this.id = id;
+        this.name = fullName;
+        this.birthDate = birthDate;
+        this.age = age;
+        this.gender = gender;
+        this.cellPhone = cellPhone;
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.cep = cep;
+        this.street = street;
+        this.houseNumber = houseNumber;
+    }
+}
+const user1 = new User('1', 'Filipe Bratti Schmidt', '04/10/1986', '38 anos', 'Masculino', '49 9 9911 3041', 'Brasil', 'Santa Catarina', 'Lages', '88511030', 'Rua João Dias Brascher', '43');
+const user2 = new User('2', 'Juliana Aparecida Duarte Schmidt', '15/05/1981', '41 anos', 'Feminino', '49 9 9916 1330', 'Brasil', 'Santa Catarina', 'Lages', '88511030', 'Rua João Dias Brascher', '43');
+console.log(user1);
+console.log(user2);
